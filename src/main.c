5a44217c23cb25100f1511d5bd48695ecf3dca83
John Lin <jlin1674@gmail.com>
2018-12-25 23:24:12 -0500

    finished parse_line

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include "shell.h"
 
 int main() {
     char *c = read_line();
     printf("%s\n", c);
+    char **tokens = parse_line(c);
+    for (int i = 0; tokens[i]; i++)
+        printf("%s\n", tokens[i]); 
     return 0;
 }
--- src/shell.c
+++ src/shell.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define BUFSIZE 1024
+#define MAXSTR 1024
+#define MAXTOKENS 1024
+#define TRUE 1
+#define FALSE 0
+
+static void append(char **tokens, int token_index, char *line);
+static char* slice(char *line, int start, int end);
 
 char *read_line(void) {
-    // + 1 for '\0'
-    char *buf = malloc(sizeof(char) * (BUFSIZE + 1));
+    char *buf = malloc(sizeof(char) * (MAXSTR + 1));
     int i, c;
     i = 0;
 
@@ -14,10 +20,10 @@ char *read_line(void) {
         exit(1);
     }
     
-    while (i < BUFSIZE && (c = getchar()) != EOF && c != '\n') 
+    while (i < MAXSTR && (c = getchar()) != EOF && c != '\n') 
         *(buf + i++) = c;
 
-    if (i >= BUFSIZE)
+    if (i >= MAXSTR)
         fprintf(stderr, "overflow");
     else
         *(buf + i) = '\0'; 
@@ -26,5 +32,37 @@ char *read_line(void) {
 } 
 
 char **parse_line(char *line) {
-    char **tokens = malloc(sizeof(char *) * BUFSIZE);
+    char **tokens = malloc(sizeof(char *) * (MAXTOKENS + 1));
+    int in_space = TRUE;
+    int i, token_start, token_index;
+    token_index = 0;
+    
+    for (i = 0; line[i] != '\0'; i++) {
+        if (in_space && !isspace(line[i])) {
+            in_space = FALSE;
+            token_start = i; 
+        }
+        else if (!in_space && isspace(line[i])) {
+            append(tokens, token_index++, slice(line, token_start, i));
+            in_space = TRUE;
+        }
+    }
+    if (!in_space)
+        append(tokens, token_index++, slice(line, token_start, i+1));
+
+    append(tokens, token_index++, NULL);
+        
+    return tokens;
+}
+
+static void append(char **tokens, int token_index, char *line) {
+    tokens[token_index] = line;
+}
+
+static char* slice(char *line, int start, int end) {
+    char *s = malloc(sizeof(char) * MAXSTR);
+    for (int i = start; i < end; i++)
+        s[i - start] = line[i]; 
+    s[end] = '\0';
+    return s;
 }
