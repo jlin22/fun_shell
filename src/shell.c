94cd8719add842cade1f00dec26f16b019ea903a
John Lin <jlin1674@gmail.com>
2018-12-26 21:32:15 -0500

    finished shell processes

--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <ctype.h> // for isspace
+#include <unistd.h> // for exec
+#include <sys/types.h> // for fork
+#include <sys/wait.h> // for waitpid
 
 #define MAXSTR 1024
 #define MAXTOKENS 1024
@@ -66,3 +69,25 @@ static char* slice(char *line, int start, int end) {
     s[end] = '\0';
     return s;
 }
+
+int launch_shell(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) { // child
+        if (execvp(args[0], args) == -1)
+            fprintf(stderr, "shell failed to run");
+        exit(1);
+    }
+    else if (pid < 0) 
+        fprintf(stderr, "failed to fork");
+    else { // parent
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+
+
